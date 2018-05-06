2f4534e9ac2421952e8f3ad27e6b6aeac50ef730
reyhanjav <reyhan.qartem@gmail.com>
2018-05-06 08:45:28 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,187 @@
+// Replace the code in main.dart with the following.
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart'; 
+import 'package:flutter/foundation.dart';
 
-void main() => runApp(new StoryApp());
+void main() {
+  runApp(new FriendlychatApp());
+}
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
 
-class StoryApp extends StatelessWidget {
-  // This widget is the root of your application.
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primaryColor: Colors.white,
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Stry'),
+      title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
+        ? kIOSTheme                                              //new
+        : kDefaultTheme,                                         //new
+      home: new ChatScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// Modify the ChatScreen class definition to extend StatefulWidget.
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class ChatScreen extends StatefulWidget {                     //modified
+  @override                                                        //new
+  State createState() => new ChatScreenState();                    //new
+} 
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+// Add the ChatScreenState class definition in main.dart.
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;                   //new
+  @override                                                        //new
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  return new Scaffold(
+    appBar: new AppBar(
+        title: new Text("Friendlychat"),
+        elevation:
+            Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0),
+    body: new Container(                                             //modified
+        child: new Column(                                           //modified
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
             ),
           ],
         ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS //new
+            ? new BoxDecoration(                                     //new
+                border: new Border(                                  //new
+                  top: new BorderSide(color: Colors.grey[200]),      //new
+                ),                                                   //new
+              )                                                      //new
+            : null),                                                 //modified
+  );
+}
+
+@override
+void dispose() {                                                   //new
+  for (ChatMessage message in _messages)                           //new
+    message.animationController.dispose();                         //new
+  super.dispose();                                                 //new
+}
+
+  
+ Widget _buildTextComposer() {
+  return new IconTheme(
+    data: new IconThemeData(color: Theme.of(context).accentColor),
+    child: new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onChanged: (String text) {          //new
+                setState(() {                     //new
+                  _isComposing = text.length > 0; //new
+                });                               //new
+              },                                  //new
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  new InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          new Container(
+   margin: new EdgeInsets.symmetric(horizontal: 4.0),
+   child: Theme.of(context).platform == TargetPlatform.iOS ?  //modified
+   new CupertinoButton(                                       //new
+     child: new Text("Send"),                                 //new
+     onPressed: _isComposing                                  //new
+         ? () =>  _handleSubmitted(_textController.text)      //new
+         : null,) :                                           //new
+   new IconButton(                                            //modified
+       icon: new Icon(Icons.send),
+       onPressed: _isComposing ?
+           () =>  _handleSubmitted(_textController.text) : null,
+       )
+   ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
+    ),
+  );
+}
+
+void _handleSubmitted(String text) {
+  _textController.clear();
+  setState(() {                                                    //new
+    _isComposing = false;                                          //new
+  });                                                              //new
+  ChatMessage message = new ChatMessage(
+    text: text,
+    animationController: new AnimationController(
+      duration: new Duration(milliseconds: 500),
+      vsync: this,
+    ),
+  );
+  setState(() {
+    _messages.insert(0, message);
+  });
+  message.animationController.forward();
+}
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(                                    //new
+    sizeFactor: new CurvedAnimation(                              //new
+        parent: animationController, curve: Curves.bounceOut),      //new
+    axisAlignment: 0.0,                                           //new
+    child: new Container(                                    //modified
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+           new Expanded(                                               //new
+  child: new Column(                                   //modified
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      new Text(_name, style: Theme.of(context).textTheme.subhead),
+      new Container(
+        margin: const EdgeInsets.only(top: 5.0),
+        child: new Text(text),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // This trailing comma makes auto-formatting nicer for build methods.
+    ],
+  ),
+),
+          ],
+        ),
+      )                                                           //new
     );
   }
 }
+
+const String _name = "Demn";
